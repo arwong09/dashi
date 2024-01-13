e80e8c2ab611965d656587f584e31d2d846ac8c9
Andrew Wong <arwong09@gmail.com>
2024-01-13 15:18:02 -0800

    Restyle login form

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,34 +3,44 @@ import Link from 'next/link'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorNotification from './ErrorNotification'
 import useLogin from '../hooks/useLogin'
+import MaxWidthLayout from '@/layouts/MaxWidthLayout'
 
 const Login = () => {
   const {
-    onEmailChange, emailValue, onPasswordChange, passwordValue, onSubmit, isLoading, error,
+    onEmailChange,
+    emailValue,
+    onPasswordChange,
+    passwordValue,
+    onSubmit,
+    isLoading,
+    error,
   } = useLogin()
 
   return (
-    <div className="min-h-full flex flex-col justify-center pb-32 sm:px-6 lg:px-8 bg-gray-50">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="w-12 h-12 relative ml-2">
-          <NextImage
-            layout="fill"
-            className="mx-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-            alt="Workflow"
-          />
+    <div className="bg-slate-50 h-full">
+      <MaxWidthLayout>
+        <div className="pt-14 pb-10 pl-6">
+          <div className="h-6 relative">
+            <NextImage
+              objectFit="contain"
+              objectPosition="left"
+              layout="fill"
+              className="mx-auto"
+              src="/logo.png"
+              alt="Workflow"
+            />
+          </div>
         </div>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <h2 className="mb-8 text-2xl font-bold text-gray-700">Sign in to your account</h2>
-          <form className="space-y-6" onSubmit={onSubmit}>
+        <div className="w-full bg-white rounded-md shadow-xl px-16 py-14">
+          <h1 className="font-semibold text-2xl text-slate-700 mb-5">
+            Sign in to your account
+          </h1>
+          <form className="space-y-8" onSubmit={onSubmit}>
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <label htmlFor="email" className="block text-sm text-slate-700">
                 Email
               </label>
-              <div className="mt-1">
+              <div className="mt-2.5">
                 <input
                   onChange={onEmailChange}
                   value={emailValue}
@@ -39,28 +49,32 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="appearance-none block w-full px-3 py-3 border border-zinc-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-indigo-200/80 focus:ring-4 focus:border-indigo-300 sm:text-sm"
                 />
               </div>
             </div>
 
             <div>
               <div className="flex justify-between w-full">
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                <label
+                  htmlFor="password"
+                  className="block text-sm text-slate-700"
+                >
                   Password
                 </label>
                 <div className="flex items-center justify-end">
                   <div className="text-sm">
-                    <Link href="/reset-password">
-                      <a className="text-indigo-600 hover:text-indigo-500">
-                        Forgot your password?
-                      </a>
+                    <Link
+                      href="/reset-password"
+                      className="text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-indigo-200/80 focus:ring-4 focus:border-indigo-300 rounded"
+                    >
+                      Forgot your password?
                     </Link>
                   </div>
                 </div>
               </div>
 
-              <div className="mt-1">
+              <div className="mt-2.5">
                 <input
                   onChange={onPasswordChange}
                   value={passwordValue}
@@ -69,7 +83,7 @@ const Login = () => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  className="appearance-none block w-full px-3 py-3 border border-zinc-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-indigo-200/80 focus:ring-4 focus:border-indigo-300 sm:text-sm"
                 />
               </div>
             </div>
@@ -78,22 +92,23 @@ const Login = () => {
               <button
                 disabled={isLoading}
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-300"
+                className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-indigo-200/80 focus:ring-4 disabled:bg-indigo-300"
               >
                 {isLoading ? <LoadingSpinner /> : 'Sign in'}
               </button>
-              <div className="mt-6 ml-2 text-sm flex justify-center w-full text-gray-700">
+              <div className="mt-6 ml-2 text-sm flex justify-center w-full text-slate-700">
                 Don&apos;t have an account?
-                <Link href="signup"><a className="ml-1 text-indigo-600 hover:text-indigo-500">Sign up</a></Link>
+                <Link
+                  href="signup"
+                  className="ml-1 text-indigo-500 hover:text-indigo-600 focus:outline-none focus:ring-indigo-200/80 focus:ring-4 focus:border-indigo-300 rounded"
+                >
+                  Sign up
+                </Link>
               </div>
             </div>
           </form>
         </div>
-
-        <div className="mt-6">
-          {error && <ErrorNotification error={error} />}
-        </div>
-      </div>
+      </MaxWidthLayout>
     </div>
   )
 }
